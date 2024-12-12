5879754fedb427fc074a35ecb92188d2c9e1117a
Irregullar <89784677+Iregullar@users.noreply.github.com>
2024-12-12 21:56:11 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("session_name", "BACpw5M0PJzZ1KiaJMTUyqfH8J0D23kPPeqV4oT5_A_SVwHj2u8R3ImvxebMzGTeDUkjsE4iqN6Gzl0xPA8elOuCBxyDeSSNuQV9CvcP1j9CgnNkUKkZrym4pD3FOZbAhiYAnh6P6no3GJWGaiom429vgmaYSgFlfqW7QILkr7Y-0F73aFOMXNCaJ6HDZbatL6ReMfTbQG0dCu5zvo7E_wPTe2GBbhnEVhS11hcyhh9_tAMVFKcwHFRX4TT6ozEzKe5oimollR9KpLMjNgBC1Q_Pp5yaUT1QA3bQvdQQPRO25LEMLMESc70W6yb9GM_fDWkryjs7mF4mP6ccXIW5_CyBAAAAAV9WO64A")
-API_ID = int(getenv("api_id", "20765181"))
-API_HASH = getenv("api_hash", "e8ec2b740ac91dfce31faa3ef654d1a4")
-CHANNEL_ID = getenv("channel_id", "-1001853114058")
+SESSION_NAME = getenv("session_name", "BAGANPEAfKgQtKlGeT_B-Pv3nZf_jY2Wiit_KyGMcm_G_32SBjaOkUNC8Kd5u-bgcJ_3fZ37Ny7MrkhJK2SqRcq-eMKMtJGl_NUVyCxiKuUVSsROqP2zwfg5YK930jbFHVe_Kmw73AmIMAdYHcS014_yVqqq0B-oJ9xJRqgYqzvrfCWcPK8fov88uK9c3iiGDHZ490G9zyCxGQJ9Vv7JdrClq8LGlawzl3BbPcQ5kAPpm2FsFSbS3DL8DPkNLdTgIWazdSIFwrOxnxdkbZNng9vNKJtkrpr7vB_N4Xymx5EyznUHU6I0sp5Mxgs-Oaq5a8cyLAYEIpA7-Kc-er81L19eLFmtfQAAAAGbo6nbAA")
+API_ID = int(getenv("api_id", "25179377"))
+API_HASH = getenv("api_hash", "8c0bd3df9ebe3750cfe0dfe34bcf53a4")
+CHANNEL_ID = getenv("channel_id", "-1002287843270")
 last_messages_amount = 50 
